13cfe933e6d56790d7d7d15e482edf4fd6328c12
YauheniRusinchuk <ruevgal@gmail.com>
2019-02-13 09:08:49 +0300

    style

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,4 +5,7 @@ from requests_html import HTMLSession
 def index(request):
     #session = HTMLSession()
     #r : set = session.get('http://google.com')
+    if request.is_ajax() and request.POST:
+        name_web_site:str = request.POST.get('name')
+        print('request : ', name_web_site)
     return render(request, 'home.html', {})
--- apps/static/js/main.js
+++ apps/static/js/main.js
@@ -3,10 +3,18 @@ $(function(){
     $('.cls_form').on('submit', (e)=>{
         e.preventDefault();
         console.log('CLICK');
-        let result = $('.input_website').val();
 
-        console.log("INPUT : ", result)
-
+        $.ajax({
+            url: $(this).attr('action'),
+            type: 'POST',
+            data: {
+                name : $('.input_website').val(),
+                csrfmiddlewaretoken : $('input[name=csrfmiddlewaretoken]').val()
+            },
+            success : function(data) {
+                console.log('Success ...')
+                $('.cls_form')[0].reset();
+            }
+        })
     })
-
 })
