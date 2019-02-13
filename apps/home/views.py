d0d57eef28c175613ab7d0ae72fa6425e1d35d55
YauheniRusinchuk <ruevgal@gmail.com>
2019-02-13 11:55:15 +0300

    add js

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import json
 from requests_html import HTMLSession
 # Create your views here.
 
 def index(request):
-    #session = HTMLSession()
+    session = HTMLSession()
     #r : set = session.get('http://google.com')
     if request.is_ajax() and request.POST:
         name_web_site:str = request.POST.get('name')
         print('request : ', name_web_site)
+        url:str = 'http://' + name_web_site
+        r = session.get(url)
+        data = {'absolute_links': list(r.html.absolute_links)}
+        return HttpResponse(json.dumps(data), content_type='application/json')
     return render(request, 'home.html', {})
--- apps/static/js/main.js
+++ apps/static/js/main.js
@@ -2,7 +2,7 @@ $(function(){
 
     $('.cls_form').on('submit', (e)=>{
         e.preventDefault();
-        console.log('CLICK');
+        $('.result_loading').text("Loading ....")
 
         $.ajax({
             url: $(this).attr('action'),
@@ -12,8 +12,22 @@ $(function(){
                 csrfmiddlewaretoken : $('input[name=csrfmiddlewaretoken]').val()
             },
             success : function(data) {
-                console.log('Success ...')
+                console.log(data['absolute_links'])
                 $('.cls_form')[0].reset();
+                $('.result_loading').hide();
+                let main_result = $('.main_result');
+                let div_abs = $('<div/>', {
+                    class: 'absolute'
+                });
+                div_abs.append("<h4> ABSOLUTE LINKS </h4>")
+                data['absolute_links'].forEach((element)=>{
+                    let newlink = document.createElement('a');
+                    newlink.innerHTML = element
+                    newlink.setAttribute('href', element);
+                    newlink.setAttribute('target', '_blank');
+                    div_abs.append(newlink)
+                })
+                main_result.append(div_abs)
             }
         })
     })
